a555e2b7f46f2847bac94101bdf32aafe4808c3e
Alex <alex@alexs-imac.local>
2020-11-25 08:51:23 -0800

    Build sin problems

--- mushroom/Controllers/ViewController.swift
+++ mushroom/Controllers/ViewController.swift
@@ -15,17 +15,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var pw: UITextField!
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
+    var items:[User]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        fetch_users()
+    }
+    
+    func fetch_users()
+    {
+        do{
+            self.items = try context.fetch(User.fetchRequest())
+        }catch{ }
     }
     
     //Functions
     @IBAction func signIn(_ sender: Any)
     {
-        
+        for  usuario in items!
+        {
+            //if username == usuario.username
+            //{
+                //if pw == usuario.pw
+                //{
+                    
+                //}
+            //}
+        }
     }
 }
 
