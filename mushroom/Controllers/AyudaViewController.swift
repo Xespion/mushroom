2374708b9d92c5af557986776f88f553bb311a50
Alonso Bravo Moyano <alobrv@gmail.com>
2020-11-25 18:38:02 +0100

    Empezada view de ayuda

---
+++ mushroom/Controllers/AyudaViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AyudaViewController.swift
+//  mushroom
+//
+//  Created by Alonso Bravo Moyano on 25/11/20.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class AyudaViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cerrar(_ sender: UIButton) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
