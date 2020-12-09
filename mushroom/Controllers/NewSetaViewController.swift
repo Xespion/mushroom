fd6c157b877039ac91fa5eac3ff2753807e9a725
Bruno <bruno@brunos-macbook-pro.local>
2020-12-09 10:54:26 -0800

    Cambios en add seta

--- mushroom/Controllers/NewSetaViewController.swift
+++ mushroom/Controllers/NewSetaViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class NewSetaViewController: UIViewController {
 
+    @IBOutlet weak var imagenEsporada: UIImageView!
+    @IBOutlet weak var mensajeSeleccion: UILabel!
+    @IBOutlet weak var Imagen9: UIImageView!
+    @IBOutlet weak var Imagen8: UIImageView!
+    @IBOutlet weak var Imagen7: UIImageView!
+    @IBOutlet weak var Imagen6: UIImageView!
+    @IBOutlet weak var Imagen5: UIImageView!
+    @IBOutlet weak var Imagen4: UIImageView!
+    @IBOutlet weak var Imagen3: UIImageView!
+    @IBOutlet weak var Imagen2: UIImageView!
+    @IBOutlet weak var Imagen1: UIImageView!
+    @IBOutlet weak var viewOloryColor: UIView!
+    @IBOutlet weak var viewNegra: UIView!
+    @IBAction func seleccionarOlor(_ sender: Any) {
+        viewNegra.isHidden = false;
+        viewOloryColor.isHidden = true;
+        mensajeSeleccion.text! = "Seleccionar olor:"
+    }
+    @IBAction func seleccionarColor(_ sender: UITapGestureRecognizer) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewNegra.isHidden = true;
+        viewOloryColor.isHidden = true;
         // Do any additional setup after loading the view.
     }
     
